f598a2d6ccf0780ee282295ca5056e0917cf4192
ascafe777 <aksdenis2021@gmail.com>
2025-08-26 00:01:04 +0300

    Create solve.s

---
+++ building-a-web-server/level-2/solve.s
@@ -0,0 +1,16 @@
+.intel_syntax noprefix
+.section .text
+.global _start
+
+_start:
+    /* socket(AF_INET, SOCK_STREAM, IPPROTO_IP) */
+    mov rax, 41        /* __NR_socket */
+    mov rdi, 2         /* AF_INET */
+    mov rsi, 1         /* SOCK_STREAM */
+    xor rdx, rdx       /* protocol = 0 */
+    syscall            /* result in rax (but we don't care about it for exit) */
+
+    /* exit(0) */
+    xor rdi, rdi       /* status = 0 */
+    mov rax, 60        /* __NR_exit */
+    syscall
